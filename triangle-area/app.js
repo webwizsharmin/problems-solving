5e05b1d1990b7ecd3ebc95435f4885c37fc573ba
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2025-08-22 09:02:13 +0600

    feat: Add program to calculate area of triangle
    This commit introduces a new program that prompts the user for the base and height
    of a triangle and calculates its area using the formula (base * height) / 2.

---
+++ triangle-area/app.js
@@ -0,0 +1,7 @@
+const b = prompt("Please enter the base of triangle: ");  //Declare variable for base & it will collect the value from the user
+
+const h = prompt("Please enter the height of triangle: ");  // Dear
+
+const area = 0.5 * b * h;
+
+console.log(`The area is: ${area}`);
