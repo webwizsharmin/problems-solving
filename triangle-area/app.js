6088aaedf580249020a25a1190b20cae1283efcf
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2025-08-22 09:05:40 +0600

    some unimportant changes

--- triangle-area/app.js
+++ triangle-area/app.js
@@ -1,6 +1,6 @@
 const b = prompt("Please enter the base of triangle: ");  //Declare variable for base & it will collect the value from the user
 
-const h = prompt("Please enter the height of triangle: ");  // Dear
+const h = prompt("Please enter the height of triangle: ");  // Declare the height
 
 const area = 0.5 * b * h;
 
