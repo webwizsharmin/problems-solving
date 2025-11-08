0d7e3c12edd02a987b005aec3ec08cfad1481b30
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2025-11-08 10:11:37 +0600

    complete the level-2

--- function/test.js
+++ function/test.js
@@ -1,3 +1,4 @@
+/* LEVEL-1: WARM-UP (BASIC) */
 // Even or Odd (level-1, 1)
 const isOddEven = function (num) {
   return num % 2 === 0 ? "Even" : "Odd";
@@ -21,3 +22,36 @@ const largestNum = function (...arr) {
 let myArr = [9, 23, 17];
 
 console.log(largestNum(...myArr));
+
+/*LEVEL-2: LOGIC CHALLENGES */
+
+// Return the unique value from the array
+const originalArray = [2, 4, 6, 3, 3, 4, 6, 5];
+const uniqueArray = getUniqueValues(originalArray);
+function getUniqueValues(arr) {
+  return [...new Set(arr)];
+}
+
+console.log(uniqueArray);
+
+// count the number of each character appears in a string.
+function countcharacters(str) {
+  const charCounts = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const char = str[i];
+
+    if (charCounts[char]) {
+      charCounts[char]++;
+    } else {
+      charCounts[char] = 1;
+    }
+  }
+
+  return charCounts;
+}
+
+const myStr = "world championship";
+const results = countcharacters(myStr);
+
+console.log(results);
